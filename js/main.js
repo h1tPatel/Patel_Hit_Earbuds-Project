5e394d2be89788bf2867f12d104795557af6b39d
Hit Patel <hitp9191@gmail.com>
2023-10-24 01:05:28 -0400

    functionality

--- js/main.js
+++ js/main.js
@@ -1,96 +1,38 @@
-// (() => {
-//   // variables
-//   const model = document.querySelector("#model");
-//   const hotspots = document.querySelectorAll(".Hotspot");
-//   const infoBoxes = [
-//     {
-//       title: "Noise-cancelling microphones",
-//       text:
-//         "Noise-cancelling microphones and a rear copper shield are optimally placed to quickly detect outside noises, working together to counter noise before it disturbs your experience.",
-//       image: "../images/hotspot.svg",
-//     },
-//     // Add more info boxes as needed
-//   ];
-
-//   // functions
-//   function modelLoaded() {
-//     hotspots.forEach((hotspot) => {
-//       hotspot.style.display = "block";
-//     });
-//   }
-
-//   function loadInfo() {
-//     infoBoxes.forEach((infoBox, index) => {
-//       // Use backticks for string interpolation
-//       let selected = document.querySelector(`#hotspot-${index + 1}`);
-      
-//       // Create the elements for title and text
-//       const titleElement = document.createElement('h2');
-//       titleElement.textContent = infoBox.title;
-      
-//       const textElement = document.createElement('p');
-//       textElement.textContent = infoBox.text;
-
-//       // Append title and text elements to the selected hotspot
-//       selected.appendChild(titleElement);
-//       selected.appendChild(textElement);
-//     });
-//   }
-
-//   function showInfo() {
-//     let selected = document.querySelector(`#${this.slot}`);
-//     gsap.to(selected, 1, { autoAlpha: 1 });
-//   }
-
-//   function hideInfo() {
-//     let selected = document.querySelector(`#${this.slot}`);
-//     gsap.to(selected, 1, { autoAlpha: 0 });
-//   }
-
-//   // Event Listeners
-//   model.addEventListener("load", modelLoaded);
-
-//   hotspots.forEach(function (hotspot) {
-//     hotspot.addEventListener("mouseover", showInfo);
-//     hotspot.addEventListener("mouseout", hideInfo);
-//   });
-// })();
 
 (() => {
 
-  // Data for hotspot content
-const hotspotData = [
-  {
-    title: "Ear Tip",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
-  },
-  {
-    title: "Charging Points",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
-  },
-  {
-    title: "Battery",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
-  },
-  {
-    title: "Active Noise Cancellation",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
-  },
-  {
-    title: "Multi Function Button",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
-  },
-  {
-    title: "Speaker",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
-  },
-];
-
-// Function to populate hotspot content
+  //h2 tags and p tags will get data from this variable
+  const hotspotData = [
+    { 
+      title: "Ear Tip",
+      text: "Crafted with precision, our ear tips provide a snug fit, ensuring maximum comfort and exceptional sound isolation. Say goodbye to discomfort and hello to immersive audio!",
+    },
+    {
+      title: "Charging Points",
+      text: "Our earbuds feature hassle-free magnetic charging points. Simply snap them into place, and they'll charge effortlessly. It's convenience redefined!",
+    },
+    {
+      title: "Battery",
+      text: "Experience all-day listening with our long-lasting battery. From your morning commute to late-night workouts, our earbuds keep the music playing.",
+    },
+    {
+      title: "Active Noise Cancellation",
+      text: "Escape into your own world with our advanced Active Noise Cancellation technology. Block out distractions and immerse yourself in your favorite tunes or podcasts.",
+    },
+    {
+      title: "Multi-Function Button",
+      text: "Control everything with a simple touch. Play, pause, skip tracks, and even answer calls without reaching for your phone. It's music control at your fingertips!",
+    },
+    {
+      title: "Speaker",
+      text: "Our earbuds feature high-quality speakers that deliver crystal-clear sound with deep bass. Get ready to be blown away by the richness and depth of your music.",
+    },
+  ];
+// this is is the function to give data to h2 and p tag
 function populateHotspotContent() {
   hotspotData.forEach((data, index) => {
-    const hotspotTitle = document.getElementById(`hotspot-title-${index + 1}`);
-    const hotspotText = document.getElementById(`hotspot-text-${index + 1}`);
+    const hotspotTitle = document.getElementById(`hotspot-title-${index + 1}`); // it will increment h2 id by 1 
+    const hotspotText = document.getElementById(`hotspot-text-${index + 1}`);   // it will increment text id by 1
 
     if (hotspotTitle && hotspotText) {
       hotspotTitle.textContent = data.title;
@@ -99,13 +41,13 @@ function populateHotspotContent() {
   });
 }
 
-// Add an event listener to load hotspot content when the page is ready
+//event listener to load hotspot content when the page is ready
 document.addEventListener("DOMContentLoaded", populateHotspotContent);
 
-// Get all hotspot elements
+// target all hotspot elements
 const hotspots = document.querySelectorAll('.Hotspot');
-
-// Attach hover event listeners to each hotspot
+//the code below is without GSAP
+// Attached hover event listeners to each hotspot
 hotspots.forEach((hotspot, index) => {
     hotspot.addEventListener('mouseover', () => {
         const annotation = hotspot.querySelector('.HotspotAnnotation');
@@ -116,5 +58,37 @@ hotspots.forEach((hotspot, index) => {
         const annotation = hotspot.querySelector('.HotspotAnnotation');
         annotation.style.display = 'none';
     });
+}); 
+
+//the code below uses GSAP for duration
+
+hotspots.forEach((hotspot, index) => {
+  const annotation = hotspot.querySelector('.HotspotAnnotation');
+  
+  // Initial state
+  gsap.set(annotation, { display: 'none' });
+
+  hotspot.addEventListener('mouseover', () => {
+      gsap.to(annotation, { display: 'block', duration: 0.5 });
+  });
+
+  hotspot.addEventListener('mouseout', () => {
+      gsap.to(annotation, { display: 'none', duration: 0.5 });
+  });
 });
+
+// hotspots.forEach((hotspot, index) => {
+//   const annotation = hotspot.querySelector('.HotspotAnnotation');
+  
+//   hotspot.addEventListener('mouseover', () => {
+//       // Use GSAP to smoothly show the content
+//       gsap.to(annotation, { opacity: 1, duration: 0.3 });
+//   });
+
+//   hotspot.addEventListener('mouseout', () => {
+//       // Use GSAP to smoothly hide the content
+//       gsap.to(annotation, { opacity: 0, duration: 0.3 });
+//   });
+// });
+
 })();
